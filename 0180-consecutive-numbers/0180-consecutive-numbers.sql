7074074a956e309cfa25551302e5da2e2a68d852
Drashtee Parmar <drashteeparmar@gmail.com>
2025-02-20 18:40:10 -0500

    Time: 1143 ms (34.13%), Space: 0B (100%) - LeetHub

---
+++ 0180-consecutive-numbers/0180-consecutive-numbers.sql
@@ -0,0 +1,7 @@
+# Write your MySQL query statement below
+SELECT DISTINCT l1.num AS ConsecutiveNums
+FROM Logs l1, Logs l2, Logs l3
+WHERE l1.num = l2.num 
+  AND l1.num = l3.num
+  AND l1.id + 1 = l2.id
+  AND l1.id + 2 = l3.id;
