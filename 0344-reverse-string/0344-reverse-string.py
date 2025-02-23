43d1655267503b2bb9a03d59641034363c9c357f
Drashtee Parmar <drashteeparmar@gmail.com>
2025-02-22 20:54:22 -0500

    Time: 0 ms (100%), Space: 19.8 MB (67.13%) - LeetHub

--- 0344-reverse-string/0344-reverse-string.py
+++ 0344-reverse-string/0344-reverse-string.py
@@ -4,9 +4,11 @@ class Solution(object):
         :type s: List[str]
         :rtype: None Do not return anything, modify s in-place instead.
         """
-        left, right = 0, len(s) - 1 # going left to right i.e. 0 to n-1
-        while left < right:
-            s[left], s[right] = s[right], s[left] #swap character
-            left += 1
-            right -= 1
-        return s
+        # left, right = 0, len(s) - 1 # going left to right i.e. 0 to n-1
+        # while left < right:
+        #     s[left], s[right] = s[right], s[left] #swap character
+        #     left += 1
+        #     right -= 1
+        # return s
+
+        s.reverse()
