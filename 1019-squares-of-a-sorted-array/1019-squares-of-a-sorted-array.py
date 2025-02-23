0038cb856dc7cef5fdcf09f193a0be3da5b8d19e
Drashtee Parmar <drashteeparmar@gmail.com>
2025-02-22 21:06:26 -0500

    Time: 15 ms (43.72%), Space: 14.1 MB (87.47%) - LeetHub

--- 1019-squares-of-a-sorted-array/1019-squares-of-a-sorted-array.py
+++ 1019-squares-of-a-sorted-array/1019-squares-of-a-sorted-array.py
@@ -4,16 +4,23 @@ class Solution(object):
         :type nums: List[int]
         :rtype: List[int]
         """
-        left, right = 0, len(nums) -1
-        result = []
+        # left, right = 0, len(nums) -1
+        # result = []
 
         
-        while left <= right:
-            if abs(nums[left]) > abs(nums[right]):
-                result.append(nums[left] ** 2)
-                left += 1
-            else:
-                result.append(nums[right] ** 2)
-                right -= 1
+        # while left <= right:
+        #     if abs(nums[left]) > abs(nums[right]):
+        #         result.append(nums[left] ** 2)
+        #         left += 1
+        #     else:
+        #         result.append(nums[right] ** 2)
+        #         right -= 1
 
-        return result[::-1]  # Reverse the result since we filled it from largest to smallest
+        # return result[::-1]  # Reverse the result since we filled it from largest to smallest
+
+
+
+        for i in range(len(nums)):
+            nums[i] = nums[i]**2
+        nums.sort()
+        return nums
