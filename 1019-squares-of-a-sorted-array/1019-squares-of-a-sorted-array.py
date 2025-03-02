662c5697774b3c14afb76aee80890c052471a9d8
Drashtee Parmar <drashteeparmar@gmail.com>
2025-03-02 14:54:34 -0500

    Time: 23 ms (12.17%), Space: 14.1 MB (87.66%) - LeetHub

--- 1019-squares-of-a-sorted-array/1019-squares-of-a-sorted-array.py
+++ 1019-squares-of-a-sorted-array/1019-squares-of-a-sorted-array.py
@@ -4,23 +4,33 @@ class Solution(object):
         :type nums: List[int]
         :rtype: List[int]
         """
-        n = len(nums)
-        result = [0] * n  # Initialize result array
-        left, right = 0, n - 1  # Two pointers at both ends
-        index = n - 1  # Fill result array from the end
-
-        while left <= right:
-            left_square = nums[left] ** 2
-            right_square = nums[right] ** 2
-
-            if left_square > right_square:
-                result[index] = left_square
-                left += 1
-            else:
-                result[index] = right_square
-                right -= 1
-
-            index -= 1  # Move to the next position
-
-        return result        
         
+#         using two pointer approach
+#         n = len(nums)
+#         result = [0] * n  # Initialize result array
+#         left, right = 0, n - 1  # Two pointers at both ends
+#         index = n - 1  # Fill result array from the end
+
+#         while left <= right:
+#             left_square = nums[left] ** 2
+#             right_square = nums[right] ** 2
+
+#             if left_square > right_square:
+#                 result[index] = left_square
+#                 left += 1
+#             else:
+#                 result[index] = right_square
+#                 right -= 1
+
+#             index -= 1  # Move to the next position
+
+#         return result        
+        
+    
+        for i in range(len(nums)):
+            nums[i] = nums[i]**2
+        
+        nums.sort()
+
+        return nums
+    
