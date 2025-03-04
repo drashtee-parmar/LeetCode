2106c259f39a00384c25136156ea097e34e18b0f
Drashtee Parmar <drashteeparmar@gmail.com>
2025-03-04 17:55:05 -0500

    Time: 13 ms (42.45%), Space: 12.5 MB (51.65%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.py
+++ 0009-palindrome-number/0009-palindrome-number.py
@@ -5,4 +5,14 @@ class Solution(object):
         :rtype: bool
         """
         
-        return str(x) == str(x)[::-1] # Reverse and compare
+        # return str(x) == str(x)[::-1] # Reverse and compare
+
+        if x < 0 or (x % 10 == 0 and x != 0):  # Negative and trailing zero cases
+            return False
+        
+        reversed_half = 0
+        while x > reversed_half:
+            reversed_half = reversed_half * 10 + x % 10  # Extract last digit
+            x //= 10  # Remove last digit from x
+        
+        return x == reversed_half or x == reversed_half // 10  # Check for even/odd cases
